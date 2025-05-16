464387363fd9836580418b77b97bf39cb9f97779
KAUSHIK-NITHI <kaushik.a.s75@kalvium.community>
2025-05-16 16:28:25 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Services from "./component/Services"
+
+function App() {
+      return(
+    <div>
+      <h1>Services card</h1>
+      <div>
+      <Services title = "good job"
+        discription = "testing"></Services>
+</div>
+    </div>
+  )
+};
+export default App
---
+++ Services.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function app (){
+ const Servics = ({title , discription}) =>{
+    return(
+        <div>
+            <h3>{title}</h3>
+            <p>-{discription}</p>
+        </div>
+ )}
+ }
+
+export default app;
